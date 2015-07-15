9bd038933194d0101b20b5906b7bb3e68e46fae8
GerardoDRM <gerardo.delarosama@udlap.mx>
2015-07-14 22:11:08 -0500

    Include an AlertDialog

--- app/src/main/java/doge/thecraftcod/stormy/MainActivity.java
+++ app/src/main/java/doge/thecraftcod/stormy/MainActivity.java
@@ -1,38 +1,81 @@
 package doge.thecraftcod.stormy;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        String apiKey = "b78b5fcd01d5ed09d2e827da3f75f035";
+        double latitude = 19.217154;
+        double longitude = -98.235677;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/"
+                + latitude + "," + longitude;
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        if (response.isSuccessful()) {
+                            alertUserAboutError();
+                        } else {
+                            System.out.println(response.body().string());
+
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        } else {
+            Toast.makeText(this, "network unavailable", Toast.LENGTH_LONG).show();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
+
+    public boolean isNetworkAvailable() {
+        boolean networkAvailable = false;
+
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            networkAvailable = true;
+        }
+        return networkAvailable;
     }
 }
